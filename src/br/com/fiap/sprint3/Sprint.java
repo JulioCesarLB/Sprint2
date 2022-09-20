24f7dfe3703902035699a275218d6cfb468efc1d
JulioCesarLB <julio.lifeintech@gmail.com>
2022-09-19 22:38:53 -0300

    INCIANDO O RANK

--- src/br/com/fiap/sprint3/Sprint.java
+++ src/br/com/fiap/sprint3/Sprint.java
@@ -5,23 +5,21 @@ import javax.swing.JOptionPane;
 public class Sprint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		int teams;
 		do {
 			teams = Integer.parseInt(JOptionPane.showInputDialog(null,
 					"Digite a quantidade de equipes que participaram do evento (até 88 equipes):"));
 			if (teams < 1) {
 				JOptionPane.showMessageDialog(null, teams + " é menor que o limite (1) \n Tente novamente!!");
-			} else if (teams>88) {
+			} else if (teams > 88) {
 				JOptionPane.showMessageDialog(null, teams + " é maior que o limite (88) \n Tente novamente!!");
 			}
 		} while (teams < 1 && teams > 88);
 
 		int combat;
 		do {
-			combat = Integer.parseInt(JOptionPane.showInputDialog(null,
-					"Digite a quantidade de combates que foram realizados:"));
+			combat = Integer.parseInt(
+					JOptionPane.showInputDialog(null, "Digite a quantidade de combates que foram realizados:"));
 			if (combat < 1) {
 				JOptionPane.showMessageDialog(null, "Não é possível digitar números negativos");
 			}
@@ -34,29 +32,30 @@ public class Sprint {
 		////////equipe|
 		int[] score = new int[teams];
 		int[] nota = new int[teams];
-		
-		tabela = insertData(combat,teams);
-		
-		mostraTabela(tabela,combat,teams);
-		
-		
-		
+
+		tabela = insertData(combat, teams);
+
+		mostraTabela(tabela, combat, teams);
+
+		tabela = ordenaTabela(tabela, teams, combat);
+
+		mostraTabela(tabela, combat, teams);
+
 	}
 
-	
-	
 	public static Object[][] insertData(int combat, int teams) {
 		Object[][] tabela = new Object[teams][combat + 3];
 
 		for (int i = 0; i < teams; i++) {
 			int notaTotal = 0;
 
-			for (int j = 0; j < combat+3; j++) {
+			for (int j = 0; j < combat + 3; j++) {
 
 				if (j == 0) {
 					int result;
 					do {
-						result = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o número da "+ (i+1)+"º equipe"));
+						result = Integer.parseInt(
+								JOptionPane.showInputDialog(null, "Digite o número da " + (i + 1) + "º equipe"));
 
 						if (result < 11 || result > 99) {
 							JOptionPane.showMessageDialog(null, "Valor inválido");
@@ -66,7 +65,7 @@ public class Sprint {
 					tabela[i][j] = result;
 
 					///// Insere o resultado dos combates
-				} else if (j >0 && j<=combat) {
+				} else if (j > 0 && j <= combat) {
 					char result;
 					do {
 						result = JOptionPane.showInputDialog(null, "Digite o resultado do " + (j) + "º combate"
@@ -79,15 +78,15 @@ public class Sprint {
 
 					} while (result != 'V' && result != 'D' && result != 'E' && result != 'v' && result != 'd'
 							&& result != 'e');
-					
+
 					tabela[i][j] = result;
 
 					// calcula a quantidade de pontos por combate
 					if (result == 'V' || result == 'v') {
-						notaTotal = notaTotal+5;
+						notaTotal = notaTotal + 5;
 
 					} else if (result == 'E' || result == 'e') {
-						notaTotal = notaTotal+3;
+						notaTotal = notaTotal + 3;
 					} else if (result == 'D' || result == 'd') {
 					}
 					////// Insere a nota total
@@ -114,15 +113,48 @@ public class Sprint {
 
 		return tabela;
 	}
-	
+
 	public static void mostraTabela(Object[][] tabela, int combat, int teams) {
-		for(int i =0; i<teams; i++) {
-			for(int j =0; j<combat+3;j++) {
-				System.out.print(tabela[i][j]+" | ");
+		for (int i = 0; i < teams; i++) {
+			for (int j = 0; j < combat + 3; j++) {
+				System.out.print(tabela[i][j] + " | ");
 			}
 			System.out.println("");
 		}
+
+	}
+
+	public static Object[][] ordenaTabela(Object[][] tabela, int teams, int combat) {
 		
+		int index = 0;
+		Object[][] ordenada = new Object[teams][combat + 3];
+
+		for (int j = 0; j < teams; j++) {
+			int valor = 0;
+			for (int i = 0; i < teams; i++) {
+				if (valor < (int) (tabela[i][(combat + 1)])) {
+					System.out.println(valor);
+					valor = (int) (tabela[i][(combat + 1)]);
+					index = i;
+
+				}else if(valor == (int) (tabela[i][(combat + 1)])) {
+					if((int) tabela[index][combat+2]> (int) (tabela[i][(combat + 2)])){
+						
+					}else if((int) tabela[index][combat+2]< (int) (tabela[i][(combat + 2)])){
+						valor = (int) (tabela[i][(combat + 1)]);
+						index =i;
+					}else {
+						
+					}
+				}
+			}
+			
+			for (int i = 0; i < combat + 3; i++) {
+				ordenada[j][i] = tabela[index][i];
+			}
+			tabela[index][(combat + 1)] = 0;
 		}
+		return ordenada;
+	}
 
 }
